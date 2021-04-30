eb197676c8a49cf38e3911b1ff27957e7e0108dd
Joshua Guo <72211490+joshuakguo@users.noreply.github.com>
2021-04-30 11:20:14 -0400

    Initial Commit

---
+++ CULaundry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CULaundry
+//
+//  Created by Joshua Guo on 4/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
